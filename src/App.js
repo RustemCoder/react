ff22a6b77104045c8404e89f4d312d62289c2c88
Rustem Aisariyev <scorus0808@gmail.com>
2023-08-13 12:26:05 -0400

    added images and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import logo from './logo.svg';
+
+//import logo from ".assets/logo.png";
+import logo from "./assets/logo.png"
 import React from "react";
 import './App.css';
 import Fruits from './components/Fruits';
 import FruitsCounter from "./components/FruitsCounter";
+import Homepage from "./components/Home";
+import Contact from "./components/Contact";
+import AboutLittleLemon from "./components/AboutLittleLemon";
+import { Routes, Route, Link } from "react-router-dom";
+import ReactPlayer from "react-player/youtube";
 function App() {
  
   const [fruits] = React.useState([
@@ -15,12 +22,34 @@ function thirdExample() {
     console.log('third example');
   };
   const fourthExample = () => console.log('fourth example');
-
+  const bird1 = new Audio( 
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3" 
+  ); 
+ 
+  const bird2 = new Audio( 
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3" 
+  ); 
+  function toggle1() { 
+    if (bird1.paused) { 
+      bird1.play(); 
+    } else { 
+      bird1.pause(); 
+    } 
+  }; 
+ 
+  function toggle2() { 
+    if (bird2.paused) { 
+      bird2.play(); 
+    } else { 
+      bird2.pause(); 
+    } 
+  }; 
   return (
     <div className="App">
-      <h1>Where should the state go?</h1>
-      <Fruits fruits = {fruits} />
-      <FruitsCounter fruits = {fruits} />
+     <button onClick={toggle1}>Caspian Tern 1</button> 
+      <button onClick={toggle2}>Caspian Tern 2</button> 
+  
+      <img src = {logo} alt = "Logo"></img>
     </div>
 
   );
